a30e90227ad4954f475e263c2298c2bfd42d147b
José Medina <49325707+manuel7923@users.noreply.github.com>
2021-10-02 06:10:26 -0500

    Beginning login screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,15 @@
-import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:vehicles_app/screens/login_screen.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return CupertinoApp(
-      title: 'Cupertino App',
-      home: CupertinoPageScaffold(
-        navigationBar: CupertinoNavigationBar(
-          middle: Text('Cupertino App Bar'),
-        ),
-        child: Center(
-          child: Container(
-            child: Text('Hello World'),
-          ),
-        ),
-      ),
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Vehicles App',
+      home: LoginScreen(),
     );
   }
 }
